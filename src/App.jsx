ddf54c3aef67d724ec4cc20b09a3cc171430f127
Robin Punnoose <robin.punnoose@protonmail.com>
2023-03-01 15:04:50 -0600

    added local storage and remove local storage

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,36 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.handlePokemonChange();
+    const pokeArray = localStorage.getItem("pokeArray");
+    if (pokeArray) {
+      this.setState({
+        pokeArray: JSON.parse(pokeArray),
+      });
+    } else {
+      this.handlePokemonChange();
+    }
   }
 
   handlePokemonChange = async () => {
     const result = await getPokemon();
     console.log(result, result.typeData);
+    localStorage.setItem("pokeArray", JSON.stringify(result.pokeArray));
     this.setState({
       pokeArray: result.pokeArray,
+    });
+  };
+
+  handleClearLocalStorage = () => {
+    localStorage.removeItem("pokeArray");
+    this.setState({
+      pokeArray: [],
     });
   };
 
   render() {
     return (
       <div className="App">
-        <h1>pokeDEX</h1>
+        <h1>the OG pokeDEX</h1>
         <PokeSearch searchField={this.state.searchField} />
         <PokeList pokeArray={this.state.pokeArray} />
       </div>
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -9,6 +9,14 @@ export default class Pokemon extends Component {
         <img id="image" src={pic} alt="" />
         <h3 id="title">{name}</h3>
         <div className="types">{types[0]}</div>
+        {/*idea for hover effect
+             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+            <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
+            <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
+            <p className='w-32 h-8 mx-auto text-center rounded-lg bg-white text-purple-700 font-bold text-lg cursor-pointer hover:bg-purple-700 hover:text-white'>
+                <Link href={link}>More Info</Link>
+            </p>
+        </div>*/}
       </div>
     );
   }
