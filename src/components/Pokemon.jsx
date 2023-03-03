50253e9e2e83396e82c4b841619eeb195dacea63
Robin Punnoose <robin.punnoose@protonmail.com>
2023-03-02 21:32:11 -0600

    updated types

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,16 +4,19 @@ import "./Pokemon.css";
 export default class Pokemon extends Component {
   render() {
     const { name, pic, types, flavorText } = this.props;
-    console.log(flavorText);
     return (
       <div className="pokemon" key={name}>
         <img id="image" src={pic} alt="" />
         <h3 id="title">{name}</h3>
-        <div className="types">{types[0]}</div>
+        <div className="types">
+          {types.map((type) => (
+            <span className="type" key={type}>
+              {type}
+            </span>
+          ))}
+        </div>
         <div className="flavor-container">
-          <div className="flavor-text-wrapper">
-            <div className="flavor-text">{flavorText}</div>
-          </div>
+          <div className="flavor-text">{flavorText}</div>
         </div>
       </div>
     );
--- src/services/api.js
+++ src/services/api.js
@@ -4,11 +4,15 @@ const getPokemon = async () => {
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
     const data = await resp.json();
     const getDetails = await fetch(data.species.url);
-    const details = await getDetails.json();
+    const response = await getDetails.text();
+    const details = JSON.parse(response);
     const name = data.name;
     const pic = data.sprites.front_default;
     const types = data.types.map((type) => type.type.name);
-    const flavorText = details.flavor_text_entries[0].flavor_text;
+    const flavorText = details.flavor_text_entries[10].flavor_text.replace(
+      /[\n\f\r]/g,
+      " "
+    );
     pokeArray.push({ name, pic, types, flavorText });
   }
   return {
